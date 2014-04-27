376729326a933ae628c72999c541cd1e3faaeb36
Pavel Kuzmitchev <gitkpg@MacBook-Air-Pavel-Kuzmitchev.local>
2014-04-27 23:23:48 +0400

    Initial commit

---
+++ AD.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+/**
+ * Created by gitkpg on 27.04.14.
+ */
+public class AD {
+
+    public static void main(String[] args) {
+
+        JFrame mainFrame = new JFrame("AD");
+        mainFrame.setSize(800, 600);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+
+    }
+
+}
